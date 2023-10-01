7df1e6a39529a9088137a4f0fb98205c25e05b82
damian.mitka <damian.mitka@HK-20>
2023-10-01 13:40:25 +0200

    Add project files.

---
+++ Pierwszy/Models/Kalkulator.cs
@@ -0,0 +1,10 @@
+﻿namespace Pierwszy.Models
+{
+    public class Kalkulator
+    {
+        public double a {  get; set; }
+        public double b { get; set; }
+
+        public string selektor { get; set; }
+    }
+}
